8b7c1b9b6715c4d1c381b4187c8cf7022c29abb1
Bhanu Singh <59247235+biasedzeus@users.noreply.github.com>
2022-09-12 11:43:14 +0530

    cart updated to new UI

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,118 +1,77 @@
 import React from "react";
-import { Container,
-    ListGroup,
-    ListGroupItem,
-    Button,
-    CardBody,
-    CardHeader,
-    Card,
-    CardFooter,
-    Col,
-    Row,
-    CardText
-
+import {
+  Container,
+  ListGroup,
+  ListGroupItem,
+  Button,
+  Card,
 } from "reactstrap";
 
-const Cart = ({cartItem, removeItem, buyNow}) => {
-    let amount = 0;
-    cartItem.forEach(item => {
-        amount = parseFloat(amount)+ parseFloat(item.productPrice);
-        
-    });
+const Cart = ({ cartItem, removeItem, buyNow }) => {
+  let amount = 0;
+  cartItem.forEach((item) => {
+    amount = parseFloat(amount) + parseFloat(item.productPrice);
+  });
 
-    return (
-        <Container fluid>
+  return (
+    <Container className="cart-container" fluid>
+      <h1 className="text-success yourcart">Cart</h1>
+      <ListGroup>
+        {cartItem.map((item) => (
+          <ListGroupItem className="listItem" key={item.id}>
+            <div className="flex-container">
+                <div className="image-prodName">
 
-            <h1 className ="text-success">
-                Your cart
+              <img className="cartimage" height={60} src={item.tinyImage} alt="prodimage" />
+              <div className="prodname-price">
+                <p className="productName">{item.productName}</p>
+                <p>Delivery charges @ 0%</p>
+              </div>
+                </div>
+                <p className="price">{item.productPrice}$</p>
+            </div>
+          </ListGroupItem>
+        ))}
+      </ListGroup>
+      {/* // If EVerything is empty */}
+      {cartItem.length >= 1 ? (
+        <Card className="card text-center mt-3">
+            <div className="subtotal">
+                <p>Subtotal</p>
+                <span>{amount}$</span>
+            </div>
+            <div className="subtotal">
+                <p>Shipping</p>
+                <span>3$</span>
+            </div>
+            <div className="total">
+                <p>Total</p>
+                <span><span className="usd">USD</span>{amount + 3}$</span>
+            </div>
+            <Button className="pay-btn" color="success" onClick={buyNow}>
+              PAY HERE
+            </Button>
+          {/* <CardHeader>Grand Total :</CardHeader>
+          <CardBody>
+            your amount for {cartItem.length} product is {amount}
+          </CardBody>
+          <CardFooter>
+            <Button color="success" onClick={buyNow}>
+              PAY HERE
+            </Button>
+          </CardFooter> */}
+        </Card>
+      ) : (
+        <div className="emptycardcontainer">
+        <div className="emptycard">
+            <h4>Cart is Empty</h4>
+            <h6>Add items to get started.</h6>
 
-            </h1>
-            <ListGroup>
-                {cartItem.map(item =>(
-                    <ListGroupItem key={item.id} >
-                        <Row>
-                            <Col>
-                            <img
+        </div>
+        </div>
+      )}
+    </Container>
+  );
+};
 
-                            height={80}
-                            src={item.tinyImage}
-                                />
-                            
-                            
-                            </Col>
-
-                            <Col className ="text-center">
-                                <div className="text-primary">
-                                    {item.productName}
-
-
-                                </div>
-                                <span>
-                                    Price:{item.productPrice}
-                                </span>
-
-                                <Button
-                                color="danger"
-                                onClick= {()=>removeItem(item)}
-
-                                >
-                                    Remove
-                                </Button>
-
-                            
-                            
-                            </Col>
-                        </Row>
-
-                    </ListGroupItem>
-
-
-                  
-
-                  
-                ))}
-
-            </ListGroup>
-            {/* // If EVerything is empty */}
-            {
-                cartItem.length >= 1 ? (
-                    <Card className ="text-center mt-3">
-                        <CardHeader>
-                            Grand Total :
-                        </CardHeader>
-                        <CardBody>
-                            your amount for {cartItem.length} product is {amount}
-                        </CardBody>
-                        <CardFooter>
-                            <Button 
-                            color = "success"
-                            onClick = {buyNow}
-                            >
-                                PAY HERE
-                            </Button>
-                        </CardFooter>
-
-                    </Card>
-                    
-
-                ) : (
-                    <h1 className ="text-warning">
-                    Cart is Empty</h1>
-                )
-            }
-
-
-
-
-        </Container>
-    )
-
-
-
-}
-
-
-
-
-export default Cart ;
-
+export default Cart;
